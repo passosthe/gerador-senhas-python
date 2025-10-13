66ddb22f4c155c7513a3ef60e1fd3a76cb62845c
passosthe <sthefanepassos@hotmail.com>
2025-10-13 11:41:44 -0300

    feat: Implementa coleta e validação do comprimento da senha

--- gerador_senha.py
+++ gerador_senha.py
@@ -4,7 +4,14 @@ def gerar_senha():
     return
 
 def iniciar_gerador():
-    return
+    while True:
+        try:
+            comprimento = int(input('Digite o comprimento da senha (mínimo 4): '))
+            if comprimento < 4:
+                print('O comprimento mínimo é 4. Tente novamente')
+                continue
+        except ValueError:
+            print('\nERRO: Por favor, digite apenas um número inteiro para o comprimento.')
 
 if __name__ == '__main__':
     iniciar_gerador()
