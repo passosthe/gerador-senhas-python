3341bc791625f3a029af41eb84d015d0b01d8efc
passosthe <sthefanepassos@hotmail.com>
2025-10-13 15:19:07 -0300

    feat: Adiciona entrada do usuário para tipos de caracteres

--- gerador_senha.py
+++ gerador_senha.py
@@ -1,15 +1,24 @@
 import random
 
-def gerar_senha():
+def gerar_senha(comprimento, usar_letras, usar_numeros, usar_simbolos):
     return
 
 def iniciar_gerador():
+    print('---- GERADOR DE SENHAS ----')
+
     while True:
         try:
             comprimento = int(input('Digite o comprimento da senha (mínimo 4): '))
             if comprimento < 4:
                 print('O comprimento mínimo é 4. Tente novamente')
                 continue
+            
+            print('\nSelecione os tipos de caracteres: ')
+            usar_letras = input("Quer incluir letras na senha? (Sim ou não)").lower() == 'sim' 
+            usar_numeros = input("Quer incluir números na senha? (Sim ou não)").lower() == 'sim' 
+            usar_simbolos = input("Quer incluir simbolos na senha? (Sim ou não)").lower() == 'sim' 
+
+
         except ValueError:
             print('\nERRO: Por favor, digite apenas um número inteiro para o comprimento.')
 
